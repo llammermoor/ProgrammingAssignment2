9ebba69bdb038933b67dc30a38a700c23a4b5ced
llammermoor <llammermoor@users.noreply.github.com>
2014-06-21 15:36:59 +0200

    Solution to Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+# These two functions allow the user to create a random square matrix
+# and to find its inverse efficiently. They can be used without 
+# specifying any arguments.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+# Function I: Creating a square matrix
 
+# You can specify three parameters: 
+# (1) S (seed), which controls the random number generation, 
+# (2) D (dimension), ie how big the matrix is supposed to be 
+# (3) I (inversion), which says whether you want to calculate 
+#     the inversion or not
+
+makeCacheMatrix <- function(S = 58462, D = 10, I = T) {
+        # We save the choice whether we want the inversion
+        choice <<- I
+        
+        # This is just to generate the matrix to be inverted:
+        set.seed(S)
+        Matrix <- matrix(rnorm(D^2), nrow=D, ncol=D, byrow = TRUE) 
+        
+        # Here we perform the inversion (if required) and cache the reusult
+        if(I == T){
+        X <- solve(Matrix) 
+        Inv <<- X
+        }
+        
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+# Function II: Finding the inverse matrix
+cacheSolve <- function(x = Matrix, ...) {
+        
+        # If we have performed the inversion already, 
+        # we just get the cached results
+        if(choice == T){
+                message("getting cached data") 
+                return(Inv)
+        }
+        
+        # In case we decided not calculate the inversion earlier 
+        # we need to calculate it now:
+        else {
+                results <- solve(x)
+                return(results)
+        }
+
 }
